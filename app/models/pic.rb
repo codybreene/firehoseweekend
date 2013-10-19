18d3d40b95e91bad656abbe0df8da0e440a33c9c
Cody Breene <Cody@cody-breenes-macbook-pro.local>
2013-10-19 18:28:47 -0400

    Add the ability to upload images

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,4 +1,7 @@
 class Pic < ActiveRecord::Base
-	attr_accessible :hour, :emotion, :learned
+	attr_accessible :hour, :emotion, :learned, :image 
   # attr_accessible :title, :body
+
+    mount_uploader :image, ImageUploader
+
 end
