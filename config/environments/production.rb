5231a7ecb2e97186216293b190df05e13e7d67cf
Cody Breene <Cody@cody-breenes-macbook-pro.local>
2013-10-20 15:05:02 -0400

    added user authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,12 @@ Testapp::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => 'codyfirehose.herokuapp.com'}
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+# http://codyfirehose.herokuapp.com/
+
+
 end
